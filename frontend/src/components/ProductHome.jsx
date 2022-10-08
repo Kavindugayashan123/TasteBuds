2bcb14300fd76ef23c9f484c16285a3d475e9849
Nadiranga <nadirangalakruwan22@gmail.com>
2022-10-08 12:54:10 +0530

    cv

--- frontend/src/components/ProductHome.jsx
+++ frontend/src/components/ProductHome.jsx
@@ -2,7 +2,7 @@ import {
   faBagShopping,
   faEye,
   faHeart,
-  faShoppingBag,
+  faShoppingBag,    
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
