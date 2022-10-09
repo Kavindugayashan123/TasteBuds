5e3d1ad0ef7779b35827d7a0209bfd40933aeaff
Kavindu <kavindugayashan408@gmail.com>
2022-10-09 20:25:22 +0530

    Added Outlet section and done api calling

---
+++ frontend/src/pages/Outlet.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import "../styles/outlet.css"
+import Footer from '../components/Footer';
+import FoodsOutlet from "../components/FoodsOutlet";
+
+const Outlet = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="o-row">
+        <div className="o-col">
+          <FoodsOutlet />
+        </div>
+      </div>
+      <Footer/>
+    </div>
+  );
+};
+
+export default Outlet;
