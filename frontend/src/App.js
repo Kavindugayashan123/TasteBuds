952fa7a42519e3e0311cb79afcbaa600858517b8
Kavindu <kavindugayashan408@gmail.com>
2022-10-09 20:22:48 +0530

    Get food list to HomePage by calling API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import { Home } from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Shop from "./pages/Shop";
+import Outlet from "./pages/Outlet";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Wish from "./pages/Wish";
 import Cart from "./pages/Cart";
+import OutletScreen from "./pages/OutletScreen";
 
 function App() {
   return (
@@ -14,10 +15,11 @@ function App() {
       <div className="App">
         <Routes>
           <Route path='/' element={ <Home />}/>
+          <Route path='/outlet/:_id' element={ <OutletScreen />}/>
           <Route path='/login' element={ <Login />}/>
           <Route path='/wish' element={ <Wish />}/>
           <Route path='/cart' element={ <Cart />}/>
-          <Route path='/shop' element={ <Shop />}/>
+          <Route path='/outlet' element={ <Outlet />}/>
           <Route path='/about' element={ <About />}/>
           <Route path='/contact' element={ <Contact />}/>
         </Routes>
--- frontend/src/components/ProductsHome.jsx
+++ frontend/src/components/ProductsHome.jsx
@@ -1,14 +1,29 @@
-import React from "react";
-import { products } from "../data";
+import React, { useEffect, useState } from "react";
+//import { products } from "../data";
 import ProductHome from "./ProductHome";
 import "../styles/productsHome.css";
+import axios from "axios";
 
 const ProductsHome = () => {
+
+  const [foods, setFoods] = useState([])
+
+  useEffect(()=> {
+    const fetchData = async () => {
+      const result = await axios.get('/api/foods');
+      setFoods(result.data);
+    }
+    fetchData();
+    
+  }, []);
+
+
+
   return (
     <div className="hps-container">
       <h2>Latest Food Items</h2>
     <div className="hps-row">
-    {products.slice(0,8).map((item) => (
+    {foods.slice(0,8).map((item) => (
         <ProductHome item={item} key={item._id} />
       ))}
     </div>
