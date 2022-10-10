f0328d89396f51128cb8aee0ca00d6c7eef18704
Kavindu <kavindugayashan408@gmail.com>
2022-10-10 20:35:51 +0530

    Changes in WhistList

--- frontend/src/components/WhistListOutlet.jsx
+++ frontend/src/components/WhistListOutlet.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { faHeart, faShoppingBag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import "../styles/foodOutlet.css";
 
 const WhistListOutlet = () => {
   return (
