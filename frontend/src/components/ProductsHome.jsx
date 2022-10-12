a605a36f45d0f0718b888e77f1de73c1f815ff06
Nadiranga <nadirangalakruwan22@gmail.com>
2022-10-12 08:54:11 +0530

    few changes

--- frontend/src/components/ProductsHome.jsx
+++ frontend/src/components/ProductsHome.jsx
@@ -6,14 +6,24 @@ import axios from "axios";
 
 const ProductsHome = () => {
   const [foods, setFoods] = useState([]);
+  const [foods, setFoods] = useState([]);
+  //const [outlets, setOutlets] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios.get("/api/foods");
       setFoods(result.data);
-    };
+    };;
     fetchData();
   }, []);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const result = await axios.get("/api/outlets");
+  //     setOutlets(result.data);
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <div className="hps-container">
@@ -23,6 +33,12 @@ const ProductsHome = () => {
           <ProductHome item={item} key={item._id} />
         ))}
       </div>
+      {/* <div className="hps-row">
+        <h>Top Food Outlets</h>
+        {outlets.slice(0, 8).map((item) => (
+          <ProductHome item={item} key={item._id} />
+        ))}
+      </div> */}
     </div>
   );
 };
